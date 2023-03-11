cf294c56db7953be127aff2aeb13498013c1da18
Avinash Lingaloo <avinashlng1080@gmail.com>
2023-03-11 23:53:44 +0400

    ✨ feat: Add new screens
    This commit adds three new screens to the app: Home, Launch, and Pin.

---
+++ app/screens/launch/index.tsx
@@ -0,0 +1,14 @@
+// Launch screen of the app
+
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const Launch = () => {
+  return (
+    <View>
+      <Text>Launch Screen</Text>
+    </View>
+  );
+};
+
+export default Launch;
