acbfb3716bf365c97d2181135f88dba9cff6811f
Avinash Lingaloo <avinashlng1080@gmail.com>
2023-03-12 13:01:39 +0400

    ✨ feat: Add balance and crypto type fields to `Account` model, and add `setBalance` and `setCryptoType` writer methods, and add name and pin fields to `User` model, and add `setName` and `setPin` writer methods

--- app/database/model/user/index.ts
+++ app/database/model/user/index.ts
@@ -1,7 +1,24 @@
 import {Model} from '@nozbe/watermelondb';
+import {text, writer} from '@nozbe/watermelondb/decorators';
 
+import UserInterface from '../../../../types/models/user';
 import {WDB_TBL_USER} from '../../../constants/table';
-
-export default class User extends Model {
+export default class User extends Model implements UserInterface {
   static table = WDB_TBL_USER;
+
+  @text('name') name: string = '';
+
+  @text('pin') pin: string = '';
+
+  @writer async setPin(hashPin: string) {
+    await this.update(u => {
+      u.pin = hashPin;
+    });
+  }
+
+  @writer async setName(name: string) {
+    await this.update(u => {
+      u.name = name;
+    });
+  }
 }
