00f4833db369be9065446c8eed8d84cd3f5dd1d3
Avinash Lingaloo <avinashlng1080@gmail.com>
2023-03-12 10:33:03 +0400

    ✨ feat: Add PIN input screen to login flow
    This commit introduces a new PIN input screen to the login flow. It replaces the previous code input screen and now allows users to enter a 6-digit PIN. The PIN is stored locally in the database once entered, and if the length of the PIN is greater than 5, the stacks auto-switch to the Home screen. Additionally, some code refactoring was done to remove unnecessary code and add styles to the components.

--- app/screens/launch/index.tsx
+++ app/screens/launch/index.tsx
@@ -1,43 +1,9 @@
-import React, {useState} from 'react';
-import {StyleSheet, Text, TextInput, View} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {StyleSheet, Text, View} from 'react-native';
 
 import Container from '../../components/container';
 import Digits from '../../components/digits';
 import NumberPad from '../../components/number_pad';
-
-const CodeInputScreen = () => {
-  const [code, setCode] = useState('');
-
-  const handlePress = (value: string | number) => {
-    if (value === '<<') {
-      setCode(code.slice(0, -1));
-    } else if (code.length < 6) {
-      setCode(code + value);
-    }
-  };
-
-  return (
-    <Container>
-      <View style={styles.container}>
-        <Text style={styles.hint}>
-          So as to further secure your data on this device
-        </Text>
-        <TextInput
-          editable={false}
-          keyboardType="number-pad"
-          maxLength={6}
-          secureTextEntry
-          style={styles.hiddenInput}
-          value={code}
-        />
-        <Digits currentLength={code.length} />
-        <NumberPad onPress={handlePress} />
-      </View>
-    </Container>
-  );
-};
-
-//fixme: add a scrollview for small devices
 
 const styles = StyleSheet.create({
   container: {
@@ -49,10 +15,44 @@ const styles = StyleSheet.create({
     fontSize: 16,
     marginBottom: 30,
   },
-  hiddenInput: {
-    height: 0,
-    width: 0,
-  },
 });
 
-export default CodeInputScreen;
+const Launch = () => {
+  const [pin, setPin] = useState('');
+
+  const handlePress = (value: string | number) => {
+    switch (true) {
+      case value === '': {
+        break;
+      }
+      case pin.length <= 5: {
+        setPin(pin + value);
+        break;
+      }
+      case value === '<<': {
+        setPin(pin.slice(0, -1));
+        break;
+      }
+    }
+  };
+
+  useEffect(() => {
+    if (pin.length > 5) {
+      //todo Store PIN locally in database and ensure the stacks auto-switch to Home screen
+    }
+  }, [pin]);
+
+  return (
+    <Container>
+      <View style={styles.container}>
+        <Text style={styles.hint}>
+          So as to further secure your data on this device
+        </Text>
+        <Digits currentLength={pin.length} />
+        <NumberPad onPress={handlePress} />
+      </View>
+    </Container>
+  );
+};
+
+export default Launch;
