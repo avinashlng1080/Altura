78edd091d93f8397ca5d3bae6454943aa40f9f7d
Avinash Lingaloo <avinashlng1080@gmail.com>
2023-03-12 14:46:54 +0400

    ✨ feat: Add navigation and authentication logic to App component
    This commit introduces a NavigationContainer and createNativeStackNavigator from @react-navigation/native to the App component, allowing for navigation between screens. It also adds authentication logic to determine whether the user is logged in or not, and conditionally renders either the HomeRoute or Launch screen based on that. The useEffect hook is used to asynchronously query the user data and setIsLoggedIn and setIsQuerying are used to keep track of the query's progress. Finally, a loader is displayed while waiting for the query to complete.

--- App.tsx
+++ App.tsx
@@ -1,9 +1,74 @@
-import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import React, {useEffect, useState} from 'react';
+import {ActivityIndicator, StyleSheet, View} from 'react-native';
 
-import {Routes} from './app/navigation';
+import Container from './app/components/container';
+import {ALTURA_PURPLE} from './app/constants/colors';
+import {LAUNCH, MAIN} from './app/constants/screens';
+import DatabaseManager from './app/database/manager';
+import {HomeRoute} from './app/navigation';
+import {queryUser} from './app/queries/user';
+import Launch from './app/screens/launch';
 
-function App(): JSX.Element {
-  return <Routes />;
-}
+DatabaseManager.init();
+
+const styles = StyleSheet.create({
+  loader: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+const Stack = createNativeStackNavigator();
+export const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+  const [querying, setIsQuerying] = useState<boolean>(true);
+
+  useEffect(() => {
+    const checkIfUserSignedIn = async () => {
+      const user = await queryUser();
+      console.log('>>>  user', {user});
+
+      setIsLoggedIn(Boolean(user));
+      setIsQuerying(false);
+    };
+    checkIfUserSignedIn();
+  }, []);
+
+  if (querying) {
+    return (
+      <View style={styles.loader}>
+        <ActivityIndicator size="large" color={ALTURA_PURPLE} />
+      </View>
+    );
+  }
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        {isLoggedIn ? (
+          <Stack.Screen
+            component={HomeRoute}
+            name={MAIN}
+            options={{headerShown: false}}
+          />
+        ) : (
+          <Stack.Screen
+            component={Launch}
+            name={LAUNCH}
+            options={{
+              title: 'Create a PIN',
+              headerStyle: {
+                backgroundColor: ALTURA_PURPLE,
+              },
+            }}
+          />
+        )}
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
 
 export default App;
