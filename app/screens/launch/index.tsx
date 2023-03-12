ebc5fd777bf282b415411517748fbac74a5af872
Avinash Lingaloo <avinashlng1080@gmail.com>
2023-03-12 22:50:00 +0400

    feat: jailbroken check

--- app/screens/launch/index.tsx
+++ app/screens/launch/index.tsx
@@ -1,5 +1,6 @@
-import React, {useEffect, useState} from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import JailMonkey from 'jail-monkey';
+import React, {useEffect, useMemo, useState} from 'react';
+import {Alert, StyleSheet, Text, View} from 'react-native';
 
 import {createUser} from '../../actions/user';
 import Container from '../../components/container';
@@ -21,6 +22,8 @@ const styles = StyleSheet.create({
 const Launch = () => {
   const [pin, setPin] = useState('');
 
+  // To be tested on physical device - it returns true on emulator
+  const isJailBroken = useMemo(() => JailMonkey.isJailBroken(), []);
   const handlePress = (value: string | number) => {
     switch (true) {
       case value === '': {
@@ -38,10 +41,36 @@ const Launch = () => {
   };
 
   useEffect(() => {
-    if (pin.length > 5) {
+    if (pin.length > 5 && !isJailBroken) {
       createUser(true);
+      //todo: store PIN in keychain
     }
-  }, [pin]);
+  }, [pin, isJailBroken]);
+
+  useEffect(() => {
+    // verify if this is a trusted device - there are so many ways to test - we won't do all of them for the demo project - https://github.com/GantMan/jail-monkey#api
+
+    if (isJailBroken) {
+      const reason = JailMonkey.jailBrokenMessage() || 'Not available';
+      const debugInfo =
+        JailMonkey.androidRootedDetectionMethods || 'Not available';
+
+      return Alert.alert(
+        'Jailbroken devices are not supported',
+        `
+        Reason: ${reason}
+        DebugInfo: ${JSON.stringify(debugInfo)}
+        `,
+        [
+          {
+            text: 'Exit app',
+            style: 'destructive',
+          },
+        ],
+        {cancelable: false},
+      );
+    }
+  }, [isJailBroken]);
 
   return (
     <Container>
