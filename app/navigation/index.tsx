0bce1f30e5bcf5c43d86e2f30d4ddef5cda189b8
Avinash Lingaloo <avinashlng1080@gmail.com>
2023-03-12 14:45:08 +0400

    ✨ feat: refactor navigation and remove unused code
    This commit refactors the navigation code, removing unused imports and variables. The `createNativeStackNavigator` and `Launch` components were removed, as they were not being used. The `ALTURA_PURPLE` constant was also removed, as it was no longer being used. Additionally, the `HomeRoute` component was exported for reuse in other parts of the application. The `isSignIn` function was also removed, as it is not yet implemented.

--- app/navigation/index.tsx
+++ app/navigation/index.tsx
@@ -1,17 +1,12 @@
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import * as React from 'react';
 
-import {ALTURA_PURPLE} from '../constants/colors';
-import {HOME, LAUNCH, MAIN, SETTINGS} from '../constants/screens';
+import {HOME, SETTINGS} from '../constants/screens';
 import Home from '../screens/home';
-import Launch from '../screens/launch';
 import Settings from '../screens/settings';
 
 const Tab = createBottomTabNavigator();
-const Stack = createNativeStackNavigator();
-const HomeRoute = () => {
+export const HomeRoute = () => {
   return (
     <Tab.Navigator>
       <Tab.Screen name={HOME} component={Home} options={{headerShown: false}} />
@@ -21,40 +16,5 @@ const HomeRoute = () => {
         options={{headerShown: false}}
       />
     </Tab.Navigator>
-  );
-};
-
-const isSignIn = () => {
-  //todo: check if user is signed in - read from database
-  // 1. check if user is signed in by querying the database
-  // 2. if user is signed in, return true
-
-  return false;
-};
-export const Routes = () => {
-  const isUserSignedIn = isSignIn();
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        {isUserSignedIn ? (
-          <Stack.Screen
-            component={HomeRoute}
-            name={MAIN}
-            options={{headerShown: false}}
-          />
-        ) : (
-          <Stack.Screen
-            component={Launch}
-            name={LAUNCH}
-            options={{
-              title: 'Create a PIN',
-              headerStyle: {
-                backgroundColor: ALTURA_PURPLE,
-              },
-            }}
-          />
-        )}
-      </Stack.Navigator>
-    </NavigationContainer>
   );
 };
