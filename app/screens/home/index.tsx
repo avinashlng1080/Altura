a00340658faacf42ecd361e30369f6f46e777163
Avinash Lingaloo <avinashlng1080@gmail.com>
2023-03-14 12:43:28 +0400

    project compiles

--- app/screens/home/index.tsx
+++ app/screens/home/index.tsx
@@ -1,67 +1,37 @@
-// Pull in the shims (BEFORE importing ethers)
-import '@ethersproject/shims';
-
-// Import the ethers library
-
-import {fromByteArray} from 'base64-js';
-import bip39 from 'bip39';
-import {toChecksumAddress} from 'ethereumjs-util';
-import hdkey from 'ethereumjs-wallet/dist/hdkey';
-import {ethers} from 'ethers';
+import {bufferToHex} from 'ethereumjs-util';
+import HDKey from 'hdkey';
 import React, {useState} from 'react';
-import {Button, Text, TextInput, View} from 'react-native';
+import {View, Text, TextInput, Button} from 'react-native';
 import Web3 from 'web3';
 
-import useAlturaNetInfo from '../../hook/useAlturaNetInfo';
+const Home = () => {
+  const [seed, setSeed] = useState('');
+  const [balance, setBalance] = useState(null);
 
-const web3 = new Web3();
-
-export default function Home() {
-  const [mnemonic, setMnemonic] = useState('');
-  const [privateKey, setPrivateKey] = useState('');
-  const [balance, setBalance] = useState('');
-  useAlturaNetInfo();
-  const handleMnemonicChange = text => {
-    setMnemonic(text);
-  };
-
-  const handleGenerateWallet = () => {
-    const seed = bip39.mnemonicToSeedSync(mnemonic);
-    const seedBase64 = fromByteArray(seed);
-    const seedBuffer = new Uint8Array(
-      seedBase64.match(/.{1,4}/g).map(bin => parseInt(bin, 16)),
+  const handleGetBalance = async () => {
+    const hdkey = HDKey.fromMasterSeed(Buffer.from(seed, 'hex'));
+    const path = "m/44'/60'/0'/0/0"; // Ethereum derivation path
+    const wallet = hdkey.derive(path).getWallet();
+    const privateKey = bufferToHex(wallet.getPrivateKey());
+    const web3 = new Web3(
+      new Web3.providers.HttpProvider(
+        'https://mainnet.infura.io/v3/YOUR_PROJECT_ID',
+      ),
     );
-    const root = hdkey.fromMasterSeed(seedBuffer);
-    const addrNode = root.derivePath("m/44'/60'/0'/0/0");
-    const privateKeyBuffer = addrNode.getWallet().getPrivateKey();
-    const privateKey = privateKeyBuffer.toString('hex');
-    setPrivateKey(privateKey);
-
-    const addressBuffer = addrNode.getWallet().getPublicKey();
-    const address = toChecksumAddress(`0x${addressBuffer.toString('hex')}`);
-    web3.eth.getBalance(address).then(balance => {
-      setBalance(web3.utils.fromWei(balance, 'ether'));
-    });
+    const address = web3.eth.accounts.privateKeyToAccount(privateKey).address;
+    const balance = await web3.eth.getBalance(address);
+    setBalance(balance);
   };
 
   return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Enter your 12-word recovery seed phrase:</Text>
-      <TextInput
-        style={{
-          height: 40,
-          width: 300,
-          borderColor: 'gray',
-          borderWidth: 1,
-          margin: 10,
-          padding: 5,
-        }}
-        onChangeText={handleMnemonicChange}
-        value={mnemonic}
-      />
-      <Button title="Generate Wallet" onPress={handleGenerateWallet} />
-      {privateKey ? <Text>Private Key: {privateKey}</Text> : null}
-      {balance ? <Text>Wallet Balance: {balance} ETH</Text> : null}
+    <View>
+      <TextInput placeholder="Enter recovery seed" onChangeText={setSeed} />
+      <Button title="Get Balance" onPress={handleGetBalance} />
+      {balance !== null && (
+        <Text>{`ETH Balance: ${web3.utils.fromWei(balance)}`}</Text>
+      )}
     </View>
   );
-}
+};
+
+export default Home;
--- index.js
+++ index.js
@@ -1,6 +1,14 @@
+import {decode, encode} from 'base-64';
 import {AppRegistry} from 'react-native';
 
 import App from './App';
 import {name as appName} from './app.json';
 
+if (!global.btoa) {
+  global.btoa = encode;
+}
+
+if (!global.atob) {
+  global.atob = decode;
+}
 AppRegistry.registerComponent(appName, () => App);
